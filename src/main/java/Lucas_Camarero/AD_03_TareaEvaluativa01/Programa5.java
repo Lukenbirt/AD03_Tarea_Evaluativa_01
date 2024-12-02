67bd7eec89519acf7ce1d1efe7cfeebb8dc939ea
Lucas Camarero <lcamarero@birt.eus>
2024-12-02 14:37:40 +0100

    Commit 5. Programa 5 creado

--- src/main/java/Lucas_Camarero/AD_03_TareaEvaluativa01/Programa5.java
+++ src/main/java/Lucas_Camarero/AD_03_TareaEvaluativa01/Programa5.java
@@ -1,13 +1,45 @@
+/*
+5.-	Ejercicio SGBD embebida y pool de conexiones.  (2 puntos)
+Además de los SGBD vistos en esta unidad, existe otro SGBD embebido de Java muy popular llamado HSQLDB.
+Crea en Eclipse un proyecto Maven en Java y añade la dependencia necesaria para ello. ¿Cuál es?
+Asimismo, otro Pool de conexiones popular es Vibur. Añade la dependencia necesaria en Maven. ¿Cuál es?
+Utiliza el DataSource específico de Vibur para reemplazar el DriverManager.getConnection.
+Necesitarás investigar y añadir una línea adicional explícitamente en comparación a H2 para que funcione correctamente.
+Por último, conéctate a una BD basada en fichero en HSQLDB.
+Si llegados a este punto connection.isValid(0) devuelve “true” el ejercicio puede darse por finalizado. 
+ */
+
 package Lucas_Camarero.AD_03_TareaEvaluativa01;
 
-import java.sql.*;
+import org.vibur.dbcp.ViburDBCPDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class Programa5 {
+    public static void main(String[] args) throws SQLException {
+        
+    	DataSource ds = createDataSource();
 
-	public static void main(String[] args) throws SQLException {
-		try(Connection c = DriverManager.getConnection("jdbc:mysql://localhost/dbeventos", "Luken", "Luken1470#")) {
-        	System.out.println( "connection.isValid(0) = " + c.isValid(0));
+        // test de conexión
+        try (Connection c = ds.getConnection()) {
+            System.out.println("connection.isValid(0) = " + c.isValid(0));
         }
-	}
-
+    }
+    
+    // método para crear el DataSource de Vibur
+    private static DataSource createDataSource() {
+        ViburDBCPDataSource ds = new ViburDBCPDataSource();
+        ds.setJdbcUrl("jdbc:hsqldb:mem");
+        
+        // usuario administrador de sistema creado por defecto
+        ds.setUsername("sa");
+        
+        // contraseña vacía creada por defecto
+        ds.setPassword("");
+        
+        // inicializa el pool de conexiones
+        ds.start();
+        return ds;
+    }
 }
